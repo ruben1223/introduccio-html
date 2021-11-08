b87b2693615f8aebfcc2a3ee8339a2a8c41e3447
ruben <you@example.com>
2021-11-08 20:21:47 +0100

    canvis

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburguer").addEventListener("click", function(){
+	document.querySelector("#main-menu").classList.toggle("active");
+	document.querySelector("#main-menu").classList.toggle("hide");
+});
