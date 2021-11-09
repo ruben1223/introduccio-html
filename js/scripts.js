2ac54a235d4ec3683df18c53d5c54b93117b0f55
ruben <rmillanr@esliceu.net>
2021-11-09 11:44:16 +0100

    menu

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".hamburguer").addEventListener("click", function(){
-	document.querySelector("#main-menu").classList.toggle("active");
-	document.querySelector("#main-menu").classList.toggle("hide");
-});
